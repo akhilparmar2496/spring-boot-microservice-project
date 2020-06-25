09dd6b3e47e9fd3b44c12f436a7e7add7208219a
akhilparmar2496 <akhilparmar2496@gmail.com>
2020-06-25 19:10:13 +0530

    Initialized 3 spring boot project

---
+++ coursecatalogueservice/src/test/java/com/microservice/coursecatalogueservice/CoursecatalogueserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.coursecatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursecatalogueserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
